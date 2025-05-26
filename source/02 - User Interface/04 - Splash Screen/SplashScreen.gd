83ef95faec00b6d7bb758d86e76b2d7297dc5fe1
Zeuk <GeekzGamingz@gmail.com>
2025-05-26 10:48:52 -0400

    Prep for Character Select

--- source/02 - User Interface/04 - Splash Screen/SplashScreen.gd
+++ source/02 - User Interface/04 - Splash Screen/SplashScreen.gd
@@ -8,6 +8,7 @@ var connected_peer: String
 @onready var MAIN: Node2D = get_tree().get_root().get_node("Main")
 @onready var NETWORK: Node2D = MAIN.get_node("Network")
 #Local Nodes
+@onready var customization: Control = $"../UI_Customization"
 @onready var text_waiting_room: TextEdit = $PopUpContainer/TabContainer/WaitingContainer/TextEdit_WaitingRoom
 #Tabs
 @onready var sub_menus: TabContainer = $HBoxContainer/SubMenus
@@ -15,6 +16,7 @@ var connected_peer: String
 @onready var tab_multiplayer: VBoxContainer = $HBoxContainer/SubMenus/Multiplayer
 @onready var tab_container: TabContainer = $PopUpContainer/TabContainer
 @onready var error_container: VBoxContainer = $PopUpContainer/TabContainer/ErrorContainer
+@onready var character_container: VBoxContainer = $PopUpContainer/TabContainer/CharacterContainer
 #------------------------------------------------------------------------------#
 #Ready Function
 func _ready() -> void:
@@ -25,16 +27,22 @@ func _ready() -> void:
 #Signaled Functions
 #Single Player
 func _on_button_single_player_button_up() -> void:
+	visibility_reset()
 	sub_menus.set_deferred("visible", true)
 	tab_singleplayer.set_deferred("visible", true)
-	tab_container.set_deferred("visible", false)
 	NETWORK.single_player = true
 #Multiplayer
 func _on_button_multiplayer_button_up() -> void:
+	visibility_reset()
 	sub_menus.set_deferred("visible", true)
 	tab_multiplayer.set_deferred("visible", true)
-	tab_container.set_deferred("visible", false)
 	NETWORK.single_player = false
+#Character
+func _on_button_character_button_up() -> void:
+	visibility_reset()
+	tab_container.set_deferred("visible", true)
+	character_container.set_deferred("visible", true)
+	customization.set_deferred("visible", true)
 #Quit
 func _on_button_quit_button_up() -> void: get_tree().quit()
 #------------------------------------------------------------------------------#
@@ -44,6 +52,14 @@ func _on_button_quit_button_up() -> void: get_tree().quit()
 func update_waiting_room(username): text_waiting_room.text += str("\n[", username, "] has joined!")
 #------------------------------------------------------------------------------#
 #Custom Signaled Functions
+#Retrieving Username
 func server_found(username): connected_peer = username
+#Update List
 func message_join(): rpc("update_waiting_room", connected_peer)
+#Error Timeout
 func message_error(): error_container.set_deferred("visible", true)
+#Visibility Toggle
+func visibility_reset():
+	sub_menus.set_deferred("visible", false)
+	tab_container.set_deferred("visible", false)
+	customization.set_deferred("visible", false)
