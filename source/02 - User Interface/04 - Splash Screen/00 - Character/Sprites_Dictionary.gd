67e42d3fd4b0507d637d861bd41a985e03193d3c
Zeuk <GeekzGamingz@gmail.com>
2025-05-31 19:02:25 -0400

    Edit Tweaks

--- source/02 - User Interface/04 - Splash Screen/00 - Character/Sprites_Dictionary.gd
+++ source/02 - User Interface/04 - Splash Screen/00 - Character/Sprites_Dictionary.gd
@@ -2,6 +2,7 @@ extends Node2D
 @onready var sprites_character: Node2D = $".."
 @onready var selection_character: HBoxContainer = $"../../../.."
 #Dictionaries
+var archived_sprite_info: Dictionary = {}
 var old_sprite_info: Dictionary = {}
 @export var sprite_info: Dictionary = {
 	"profile": String(""),
@@ -22,13 +23,22 @@ var old_sprite_info: Dictionary = {}
 	"beard": int(0),
 	"beard_color": String("Button_Color1")
 }
+#------------------------------------------------------------------------------#
+#Ready Function
+func _ready() -> void: archived_sprite_info = sprite_info
+#------------------------------------------------------------------------------#
+#Custom Functions
+#Update Dictionary
 func update_dictionary(mode):
 	var counter = selection_character.character_counter
 	var profiles = selection_character.save_container.button_save.profiles
 	selection_character.is_new = true
 	match(mode):
 		"New":
+			#Counters
 			sprites_character.sprite_base.height_counter = int(1)
+			sprites_character.is_chub = false
+			sprites_character.is_wheels = false
 			sprites_character.sprite_arm_right.arm_right_counter = int(1)
 			sprites_character.sprite_arm_left.arm_left_counter = int(1)
 			sprites_character.sprite_leg_right.leg_right_counter = int(1)
@@ -36,8 +46,12 @@ func update_dictionary(mode):
 			sprites_character.sprite_ears.ear_counter = int(0)
 			sprites_character.sprite_hair.hair_counter = int(0)
 			sprites_character.sprite_beard.beard_counter = int(0)
+			#Sprite Info
+			sprite_info = archived_sprite_info
 		"Edit":
 			sprites_character.sprite_base.height_counter = int(profiles[counter].get("height"))
+			sprites_character.is_chub = profiles[counter].get("chub")
+			sprites_character.is_wheels = profiles[counter].get("wheelchair")
 			sprites_character.sprite_arm_right.arm_right_counter = int(profiles[counter].get("arm_right"))
 			sprites_character.sprite_arm_left.arm_left_counter = int(profiles[counter].get("arm_left"))
 			sprites_character.sprite_leg_right.leg_right_counter = int(profiles[counter].get("leg_right"))
@@ -45,3 +59,4 @@ func update_dictionary(mode):
 			sprites_character.sprite_ears.ear_counter = int(profiles[counter].get("ears"))
 			sprites_character.sprite_hair.hair_counter = int(profiles[counter].get("hair"))
 			sprites_character.sprite_beard.beard_counter = int(profiles[counter].get("beard"))
+			
