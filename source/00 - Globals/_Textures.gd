169d1f67eac35e8d41f25ae0464aa99742acebd0
Zeuk <GeekzGamingz@gmail.com>
2025-06-29 12:17:01 -0400

    NPC Picker Implemented as Editor Tool

--- source/00 - Globals/_Textures.gd
+++ source/00 - Globals/_Textures.gd
@@ -3,7 +3,7 @@ extends Node2D
 #------------------------------------------------------------------------------#
 #Global Dictionaries
 #Zone Textures
-var ZONE_TEXTURES: Dictionary = {
+var ZONE: Dictionary = {
 	"ZONE_16x16": preload("res://assets/02 - Objects/00 - Buildings/00 - Templates/Template-16.png"),
 	"ZONE_16x32": preload("res://assets/02 - Objects/00 - Buildings/00 - Templates/Template-16x32.png"),
 	"ZONE_16x48": preload("res://assets/02 - Objects/00 - Buildings/00 - Templates/Template-16x48.png"),
@@ -21,11 +21,43 @@ var ZONE_TEXTURES: Dictionary = {
 	"ZONE_64x48": preload("res://assets/02 - Objects/00 - Buildings/00 - Templates/Template-64x48.png"),
 	"ZONE_64x64": preload("res://assets/02 - Objects/00 - Buildings/00 - Templates/Template-64.png")
 }
-var BODY_TEXTURES: Dictionary = {
+var TORSO: Dictionary = {
 	"S_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/00 - Torso/00 - Short/00 - Average/torso_short_average_nw.png"),
 	"S_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/00 - Torso/00 - Short/01 - Chub/torso_short_chub_nw.png"),
 	"A_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/00 - Torso/01 - Average/00 - Average/torso_average_average_nw.png"),
 	"A_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/00 - Torso/01 - Average/01 - Chub/torso_average_chub_nw.png"),
 	"T_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/00 - Torso/02 - Tall/00 - Average/torso_tall_average_nw.png"),
 	"T_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/00 - Torso/02 - Tall/01 - Chub/torso_tall_chub_nw.png")
+}
+var ARM_L: Dictionary = {
+	"S_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/01 - ArmLeft/00 - Short/00 - Average/arm_left_short_average_nw.png"),
+	"S_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/01 - ArmLeft/00 - Short/01 - Chub/arm_left_short_chub_nw.png"),
+	"A_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/01 - ArmLeft/01 - Average/00 - Average/arm_left_average_average_nw.png"),
+	"A_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/01 - ArmLeft/01 - Average/01 - Chub/arm_left_average_chub_nw.png"),
+	"T_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/01 - ArmLeft/02 - Tall/00 - Average/arm_left_tall_average_nw.png"),
+	"T_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/01 - ArmLeft/02 - Tall/01 - Chub/arm_left_tall_chub_nw.png"),
+}
+var ARM_R: Dictionary = {
+	"S_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/02 - ArmRight/00 - Short/00 - Average/arm_right_short_average_nw.png"),
+	"S_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/02 - ArmRight/00 - Short/01 - Chub/arm_right_short_chub_nw.png"),
+	"A_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/02 - ArmRight/01 - Average/00 - Average/arm_right_average_average_nw.png"),
+	"A_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/02 - ArmRight/01 - Average/01 - Chub/arm_right_average_chub_nw.png"),
+	"T_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/02 - ArmRight/02 - Tall/00 - Average/arm_right_tall_average_nw.png"),
+	"T_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/02 - ArmRight/02 - Tall/01 - Chub/arm_right_tall_chub_nw.png"),
+}
+var LEG_L: Dictionary = {
+	"S_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/03 - LegLeft/00 - Short/00 - Average/leg_left_short_average_nw.png"),
+	"S_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/03 - LegLeft/00 - Short/01 - Chub/leg_left_short_chub_nw.png"),
+	"A_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/03 - LegLeft/01 - Average/00 - Average/leg_left_average_average_nw.png"),
+	"A_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/03 - LegLeft/01 - Average/01 - Chub/leg_left_average_chub_nw.png"),
+	"T_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/03 - LegLeft/02 - Tall/00 - Average/leg_left_tall_average_nw.png"),
+	"T_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/03 - LegLeft/02 - Tall/01 - Chub/leg_left_tall_chub_nw.png"),
+}
+var LEG_R: Dictionary = {
+	"S_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/04 - LegRight/00 - Short/00 - Average/leg_right_short_average_nw.png"),
+	"S_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/04 - LegRight/00 - Short/01 - Chub/leg_right_short_chub_nw.png"),
+	"A_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/04 - LegRight/01 - Average/00 - Average/leg_right_average_average_nw.png"),
+	"A_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/04 - LegRight/01 - Average/01 - Chub/leg_right_average_chub_nw.png"),
+	"T_A_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/04 - LegRight/02 - Tall/00 - Average/leg_right_tall_average_nw.png"),
+	"T_C_NW": preload("res://assets/03 - Entities/00 - Humanoid/00 - Body/04 - LegRight/02 - Tall/01 - Chub/leg_right_tall_chub_nw.png"),
 }
--- source/03 - Entities/00 - Humanoid/02 - NPCs/NPC_Customization.gd
+++ source/03 - Entities/00 - Humanoid/02 - NPCs/NPC_Customization.gd
@@ -1,14 +1,25 @@
 @tool
-
 extends SpriteTextures
-
+#------------------------------------------------------------------------------#
+#Variables
+#Exported Variables
+#Exported Enumerations
 @export_enum("Short", "Average", "Tall") var height: String = "Average":
 	set(new_setting):
 		height = new_setting
 		update_sprites()
-@export var is_chub: bool = false
-@export var is_wheels: bool = false
-
+#Exported Bools
+@export var is_chub: bool = false:
+	set(new_setting):
+		is_chub = new_setting
+		update_sprites()
+@export var is_wheels: bool = false:
+	set(new_setting):
+		is_wheels = new_setting
+		update_sprites()
+#------------------------------------------------------------------------------#
+#Custom Functions
+#Update Sprites
 func update_sprites():
 	var body: Node2D = $"../../Sprites/Sprites_Body"
 	var offset: float
@@ -18,14 +29,31 @@ func update_sprites():
 		body.get_node("Sprite_Beard"),
 		body.get_node("Sprite_Bangs")
 	]
-	match(height):
-		"Short":
-			body.get_node("Sprite_Base").texture = BODY_TEXTURES["S_A_NW"]
-			offset = -14.0
-		"Average":
-			body.get_node("Sprite_Base").texture = BODY_TEXTURES["A_A_NW"]
-			offset = -16.0
-		"Tall":
-			body.get_node("Sprite_Base").texture = BODY_TEXTURES["T_A_NW"]
-			offset = -18.0
+	var sprite_string: String = ""
+	match(is_chub):
+		false: match(height):
+			"Short":
+				sprite_string = "S_A_NW"
+				offset = -14.0
+			"Average":
+				sprite_string = "A_A_NW"
+				offset = -16.0
+			"Tall":
+				sprite_string = "T_A_NW"
+				offset = -18.0
+		true: match(height):
+			"Short":
+				sprite_string = "S_C_NW"
+				offset = -14.0
+			"Average":
+				sprite_string = "A_C_NW"
+				offset = -16.0
+			"Tall":
+				sprite_string = "T_C_NW"
+				offset = -18.0
+	body.get_node("Sprite_Base").texture = TORSO[sprite_string]
+	body.get_node("Sprite_ArmLeft").texture = ARM_L[sprite_string]
+	body.get_node("Sprite_ArmRight").texture = ARM_R[sprite_string]
+	body.get_node("Sprite_LegLeft").texture = LEG_L[sprite_string]
+	body.get_node("Sprite_LegRight").texture = LEG_R[sprite_string]
 	for sprite in offset_sprites: sprite.offset.y = offset
