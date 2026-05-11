68b15b2c7ef43040b0a1a3228f3f34527bbc40a4
Zeuk <GeekzGamingz@gmail.com>
2026-05-11 06:10:51 -0400

    Hotbar Progress

--- source/03 - Entities/00 - Humanoid/01 - Player/00 - Scripts/Player_Input.gd
+++ source/03 - Entities/00 - Humanoid/01 - Player/00 - Scripts/Player_Input.gd
@@ -63,28 +63,42 @@ func handle_pathing() -> void:
 		if navi.avoidance_enabled: navi.set_velocity(new_velocity)
 		else: _on_navi_velocity_computed(new_velocity)
 		e.direction = round(to_local(navi.get_next_path_position()).normalized())
+#------------------------------------------------------------------------------#
 #Object Interaction
 func interact_object() -> void:
-	for o in object_detection.get_children():
+	for o in object_detection.get_children(): #Detect Raycasts
 		if o.is_colliding():
 			var object = o.get_collider().get_node("../..")
-			check_duplicates(object)
+			check_slots(object)
+			var hotbar_selected = HOTBAR.hotbar_array[HOTBAR.hotbar_selection]
 			object.rpc("interact")
-			if object.is_obtainable:
-				var hotbar_selected = HOTBAR.hotbar_array[HOTBAR.hotbar_selection]
-				if hotbar_selected.is_empty:
-					hotbar_selected.is_empty = false
-					hotbar_selected.quantity += 1
-					hotbar_selected.texture_object.texture = object.sprite_hotbar.texture
-					var object_scene = object.duplicate()
-					hotbar_selected.held_item = object_scene
-				else:
-					var group = object.get_groups()
-					if hotbar_selected.held_item.is_in_group(group[0]):
-						if object.is_stackable: hotbar_selected.quantity += 1
-				object.queue_free()
-			break
+			if object.is_obtainable: obtain_object(object, hotbar_selected)
+			break #Break Raycast Loop
 #Check Duplicate Item
-func check_duplicates(object):
-	for slot in HOTBAR.hotbar_array: if slot.contents != "Empty":
-		if slot.contents.contains(object.get_groups()[0]): HOTBAR.scroll_hotbar(slot.name)
+func check_slots(object):
+	for slot in HOTBAR.hotbar_array:
+		if slot.contents != "Empty":
+			if slot.contents.contains(object.get_groups()[0]):
+				HOTBAR.scroll_hotbar(slot.name)
+			else: find_slot()
+func find_slot():
+	for slot in HOTBAR.hotbar_array:
+		if slot.contents == "Empty":
+			HOTBAR.scroll_hotbar(slot.name)
+			break
+#Obtain Object
+func obtain_object(object, hotbar_selected):
+	if hotbar_selected.is_empty: addto_hotbar(object, hotbar_selected)
+	else:
+		var group = object.get_groups()
+		if hotbar_selected.held_item.is_in_group(group[0]):
+			if object.is_stackable: hotbar_selected.quantity += 1
+	object.queue_free()
+#Add to Hotbar
+func addto_hotbar(object, hotbar_selected):
+	hotbar_selected.is_empty = false
+	hotbar_selected.quantity += 1
+	hotbar_selected.texture_object.texture = object.sprite_hotbar.texture
+	var object_scene = object.duplicate()
+	hotbar_selected.held_item = object_scene
+	
