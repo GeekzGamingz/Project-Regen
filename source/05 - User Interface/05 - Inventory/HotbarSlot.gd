f567781ec7de8399b1fe89c7bbea8fbd5e452062
Zeuk <GeekzGamingz@gmail.com>
2026-05-11 22:43:13 -0400

    Hover to Select on Hotbar

--- source/05 - User Interface/05 - Inventory/HotbarSlot.gd
+++ source/05 - User Interface/05 - Inventory/HotbarSlot.gd
@@ -10,6 +10,7 @@ var contents: String
 #Resources
 var held_item: Object = null
 #OnReady Variables
+@onready var hotbar: PanelContainer = $"../.."
 #Slot Nodes
 @onready var texture_object: TextureRect = $Texture_Object
 @onready var line_quantity: LineEdit = $LineEdit_Quantity
@@ -20,3 +21,7 @@ func _process(_delta: float) -> void:
 		contents = held_item.name
 		line_quantity.set_deferred("visible", held_item.is_stackable)
 		line_quantity.text = str(quantity)
+
+
+func _on_mouse_entered() -> void:
+	hotbar.scroll_hotbar(name)
