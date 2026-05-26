7e655c41ad954ddacca0db6cdb3c413f3c077e4d
Zeuk <GeekzGamingz@gmail.com>
2026-05-26 04:03:07 -0400

    Added Basic Zoom

--- source/01 - Components/Camera_Main.gd
+++ source/01 - Components/Camera_Main.gd
@@ -8,6 +8,7 @@ var is_panning: bool = false
 #------------------------------------------------------------------------------#
 #Input Events
 func _input(event: InputEvent) -> void:
+	#Basic Panning
 	if event.is_action_pressed("move_camera"):
 		is_panning = true
 		global_position = MAIN.BLUEPRINT.position
@@ -16,5 +17,6 @@ func _input(event: InputEvent) -> void:
 		is_panning = false
 		if !G.IS_BUILDING: Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		else: Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+	#Basic Zoom
 	if event.is_action_pressed("zoom_in"): if zoom >= Vector2(0.3, 0.3): zoom -= Vector2(0.2, 0.2)
 	if event.is_action_pressed("zoom_out"): if zoom <= Vector2(3, 3): zoom += Vector2(0.2, 0.2)
