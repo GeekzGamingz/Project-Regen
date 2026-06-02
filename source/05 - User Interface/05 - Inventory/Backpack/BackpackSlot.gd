3466871c682fa31714c72cd9c6b2a435c479b9a9
Zeuk <GeekzGamingz@gmail.com>
2026-06-02 08:19:14 -0400

    Backpack Slot Restructure

--- source/05 - User Interface/05 - Inventory/Backpack/BackpackSlot.gd
+++ source/05 - User Interface/05 - Inventory/Backpack/BackpackSlot.gd
@@ -13,6 +13,7 @@ var mouse_hovering: bool = false
 func _on_slot_entered() -> void:
 	mouse_hovering = true
 	selection_held.set_deferred("visible", true)
+	print(name)
 
 func _on_slot_exited() -> void:
 	mouse_hovering = false
